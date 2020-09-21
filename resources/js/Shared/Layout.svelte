04abc082a1912f99bac1e9ea03dd12e825109578
Alex <maisilex7@gmail.com>
2020-09-21 16:45:36 +0300

    Updates

--- resources/js/Shared/Layout.svelte
+++ resources/js/Shared/Layout.svelte
@@ -1,19 +1,21 @@
 <script>
-    import { inertia } from '@inertiajs/inertia-svelte'
+  import { inertia } from '@inertiajs/inertia-svelte'
+
+  export let title
 </script>
   
-  <!-- <svelte:head>
-      <title>{$title ? `${$title} - Awesome App` : 'Awesome App'}</title>
-  </svelte:head> -->
+<svelte:head>
+  <title>{$title ? `${$title} - Awesome App` : 'Awesome App'}</title>
+</svelte:head>
   
-  <main>
-    <header>
-      <a use:inertia href="/">home</a>
-      <a use:inertia href="/about">about</a>
-      <a use:inertia href="/contact">contact</a>
-    </header>
+<main>
+  <header>
+    <a use:inertia href="/">home</a>
+    <a use:inertia href="/about">about</a>
+    <a use:inertia href="/contact">contact</a>
+  </header>
   
-    <article>
-      <slot />
-    </article>
-  </main>
+  <article>
+    <slot />
+  </article>
+</main>
